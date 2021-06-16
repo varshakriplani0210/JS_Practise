45c8bc8a1557ff221f707406e717359984e24c7a
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-06-16 23:40:52 +0530

    Switch Statement in JS

---
+++ Switch_Statements.js
@@ -0,0 +1,12 @@
+//Switch Statment
+let col='blue';
+switch(col){
+  case 'red':
+  console.log("i love red");
+  break;
+  case 'blue':
+  console.log("I love Blue"); //This statement is executed
+  break;
+  default:
+  console.log("i dont like colors"); // if no red , blue then this statement is executed
+}
