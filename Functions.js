c1401587e9a0b675cd76f3eb50eb218c23f4b62d
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-06-26 18:38:51 +0530

    Anonymous Functions in JS

---
+++ Functions.js
@@ -0,0 +1,14 @@
+//function declaration
+function calAge(age)
+{
+return 2037-age;
+}
+console.log(calAge(1996));
+
+//Anonymous Function or function expression
+const findAge=function (birthyear)
+{
+return 2037-birthyear;
+}
+const resultAge= findAge(1996)
+console.log(resultAge);
